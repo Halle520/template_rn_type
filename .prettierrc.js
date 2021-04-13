a218ffeab87f92c2ca8a431b1dba8ebe1a85d95c
Quy <admin@HCM00284.local>
2021-04-13 09:17:44 +0700

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
